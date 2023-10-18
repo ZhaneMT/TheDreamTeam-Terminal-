fda1dc749f05a25132a1c709ba37e1ac96726040
Ambirr Momon <ajm2992@icloud.com>
2023-10-18 16:37:39 -0400

    i added something yall lol

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 func chapterTwo() {
-    print("Coding is hard its cool though")
+    print("Coding is hard its cool though.what's the deal peeps? Check on your mental after this it was deep!")
     // Your portion of the story goes here
 }
