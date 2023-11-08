312f1090cb2f72f560557cd4c6c5002d0475c346
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-11-08 14:26:34 -0500

    Update Chapter2.swift

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -1,18 +1,16 @@
 import Foundation
-//
-//
-//Rising Action: She loved people that were down low, she meets a hood guy that started to ask for money and run game on her. He came off as clean and professional, turns out he was a women beater and manipulator for money.
+
 func chapterTwo() {
-    let chapters:[String] = ["Chapter 1", "Chapter 2", "Chapter 3", "Chapter 4"]
-    let herWeeklyRoutine = ["lash appointment", "fasting", "facials"]
-    func backstory() {
-        print(" Everyone have experienced childhood things that they may have changed due to an adult or probably even still hold those habits with them. Tasha was a seafood lover and enjoyed working out for her apperance. Although as she got older and made changes to her personal life, her eating habits changed from seafood to vegan.Eventually she gained balance in both areas eating,working out and fasting, after a bad break up cause self-recoginiton. ")
-    }
-    
     print("#################################################")
     print("\(chapters[1])")
     print("#################################################")
     
+    let chapters:[String] = ["Chapter 1", "Chapter 2", "Chapter 3", "Chapter 4"]
+    let herWeeklyRoutine = ["lash appointment", "fasting", "facials"]
+    
+    func backstory() {
+        print("Everyone have experienced childhood things that they may have changed due to an adult or probably even still hold those habits with them. Tasha was a seafood lover and enjoyed working out for her apperance. Although as she got older and made changes to her personal life, her eating habits changed from seafood to vegan.Eventually she gained balance in both areas eating,working out and fasting, after a bad break up cause self-recoginiton. ")
+    }
     
     enum favoriteSeafood {
         case Lobstertail, shrimp, crablegs, salmon
@@ -31,14 +29,15 @@ func chapterTwo() {
             print ("friday nights her and shawn speacialty was to make crablegs with boil eggs")
         }
     }
-        //loop is right here, the things that are changing is the day of the week and the activity
-    //function with input output
+    
+    //Function with input output. Loop is right here, the things that are changing is the day of the week and the activity
     func weeklyRountine(activity1:String, activity2:String, activity3:String) {
         print("mondays she does her \(activity1)")
         print("tuesday she usually \(activity2)")
         print("wednesday she does her skincare for \(activity3)")
     }
-    //------> below shows example of optional/ user input yes and output is backstory
+    
+    // This is an example of optional/ user input yes and output is backstory
     print("Do you want to hear the backstory?")
     if let hearBackStory = readLine() {
        //Conditionals---> If or else are the conditional statements, if user says yes to backstory they get a backstory if else, they move to te main plot.
