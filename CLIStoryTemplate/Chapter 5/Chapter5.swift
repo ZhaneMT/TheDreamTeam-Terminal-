f6ce48479dc8f2cc8c2d84922d5b799573e53d5f
Zhane Moorman-Taylor <zhaneworkmt@gmail.com>
2023-11-08 13:51:28 -0500

    his is the code for 5

--- CLIStoryTemplate/Chapter 5/Chapter5.swift
+++ CLIStoryTemplate/Chapter 5/Chapter5.swift
@@ -1,12 +1,27 @@
-//
-//  Chapter5.swift
 //  CLIStoryTemplate
 //
-
 import Foundation
 
 func chapterFive() {
-    // Your portion of the story goes here
+    
+    enum SituationPerspective: CaseIterable {
+        case positive, negative, neutral
+    }
+    
+    func choosePerspective(perspective: SituationPerspective) {
+        switch perspective {
+        case .positive:
+            print("She had a great time and she knows better moving forward")
+        case .negative:
+            print("She will never spend her own money until she's married")
+        case .neutral:
+            print("She made a mistake, won't do it again but enjoyed the time she had")
+        }
+    }
+    
+    if let randomPerspective = SituationPerspective.allCases.randomElement() {
+        choosePerspective(perspective: randomPerspective)
+    }
 }
 
- 
+
