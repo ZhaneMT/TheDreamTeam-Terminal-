095bf10e3cd3c96864757756c3b9b9ecc5344f6b
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-11-08 15:22:37 -0500

    Update Chapter2.swift

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -26,15 +26,15 @@ func chapterTwo() {
     //Function to choose a food
     func chooseseafood(herfavoriteseafood:favoriteSeafood) {
         switch(herfavoriteseafood) {
-        case .Lobstertail:
-            print("she always prefered to cook fried lobstertail for their date night ")
             
+        case .Lobstertail: 
+            print("she always prefered to cook fried lobstertail for their date night ") //Case for when lobster is selected
         case .shrimp:
-            print ("she prefer to have seasoned fresh shrimp over fried shrimp")
+            print ("she prefer to have seasoned fresh shrimp over fried shrimp") //Case for when shrimp is selected
         case .salmon:
-            print ("she only made stuffed salmon with cream cheese and spinach")
+            print ("she only made stuffed salmon with cream cheese and spinach") //Case for when salmon is selected
         case.crablegs:
-            print ("friday nights her and shawn speacialty was to make crablegs with boil eggs")
+            print ("friday nights her and shawn speacialty was to make crablegs with boil eggs") //Case for when crablegs is selected
         }
     }
     
@@ -48,29 +48,42 @@ func chapterTwo() {
     //Ask if a read wants to hear the backstory
     print("Do you want to hear the backstory?")
 
-    //Conditional to let the user choose if they want to hear the backstory
+    //Optional to let the user choose if they want to hear the backstory
     if let hearBackStory = readLine() {
 
-        //Condition for if the user does want to hear hear the backstory
+        //Condition for if the user wants to hear hear the backstory
         if (hearBackStory == "yes") {
+            
+            //Call the function for the bckstory
             backstory()
+
+            //Run the print statements for the stories
             print(" Tasha had much balance in her life, before dating and getting to know people. Also she was a family person she always understood the importance of family structure. However tasha love hard like most people and forgot about her weekly rountine working out and doing her therapy yogo. However her favorite food is seafood, she find herself very happy while watching her figure and staying at a weight of 175 before the break up.")
-            //Refrence to struct in main
             print("\(person1.name) love people that were down to earth")
             print("\(person1.name) meets a hood guy that started to ask for money and run game on her")
             print("He came off as clean and Professional")
             print("Turns out he was abusive and Manipulator")
+
+            //Call the function to choose her favorite seafood
             chooseseafood(herfavoriteseafood: .Lobstertail)
+
+            //Call the function for her weekly routine
             weeklyRountine(activity1: herWeeklyRoutine[0], activity2: herWeeklyRoutine[1], activity3: herWeeklyRoutine[2] )
 
-        //Condition for if the user doesnt want to hear te backstory
+        //Condition for if the user doesnt want to hear the backstory
         } else { 
+            
+            //Run the print statements for the stories
             print(" Tasha had much balance in her life, before dating and getting to know people. Also she was a family person she always understood the importance of family structure. However tasha love hard like most people and forgot about her weekly rountine working out and doing her therapy yogo. However her favorite food is seafood, she find herself very happy while watching her figure and staying at a weight of 175 before the break up.")
             print("\(person1.name) love people that were down to earth")
             print("\(person1.name) meets a hood guy that started to ask for money and run game on her")
             print("He came off as clean and Professional")
             print("Turns out he was abusive and Manipulator")
+
+            //Call the function to choose her favorite seafood
             chooseseafood(herfavoriteseafood: .Lobstertail)
+
+            //Call the function for her weekly routine
             weeklyRountine(activity1: herWeeklyRoutine[0], activity2: herWeeklyRoutine[1], activity3: herWeeklyRoutine[2] )
         }
     }
