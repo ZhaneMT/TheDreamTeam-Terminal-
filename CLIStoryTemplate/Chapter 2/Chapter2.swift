c451b96d8eeb4462cf97a838c4efe12f494c515d
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-11-08 14:39:17 -0500

    Update Chapter2.swift

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -1,21 +1,25 @@
 import Foundation
 
 func chapterTwo() {
+    let currentChapter = 2
     print("#################################################")
-    print("\(chapters[1])")
+    print("\(currentChapter)")
     print("#################################################")
-    
-    let chapters:[String] = ["Chapter 1", "Chapter 2", "Chapter 3", "Chapter 4"]
+
+    //Array for things she does during the week
     let herWeeklyRoutine = ["lash appointment", "fasting", "facials"]
-    
+
+    //Function to print the backstory
     func backstory() {
         print("Everyone have experienced childhood things that they may have changed due to an adult or probably even still hold those habits with them. Tasha was a seafood lover and enjoyed working out for her apperance. Although as she got older and made changes to her personal life, her eating habits changed from seafood to vegan.Eventually she gained balance in both areas eating,working out and fasting, after a bad break up cause self-recoginiton. ")
     }
-    
+
+    //Enum for her favorite kinds of seafood
     enum favoriteSeafood {
         case Lobstertail, shrimp, crablegs, salmon
     }
-    
+
+    //Function to choose a food
     func chooseseafood(herfavoriteseafood:favoriteSeafood) {
         switch(herfavoriteseafood) {
         case .Lobstertail:
@@ -30,17 +34,20 @@ func chapterTwo() {
         }
     }
     
-    //Function with input output. Loop is right here, the things that are changing is the day of the week and the activity
+    //Function for her weekly routine
     func weeklyRountine(activity1:String, activity2:String, activity3:String) {
         print("mondays she does her \(activity1)")
         print("tuesday she usually \(activity2)")
         print("wednesday she does her skincare for \(activity3)")
     }
     
-    // This is an example of optional/ user input yes and output is backstory
+    //Ask if a read wants to hear the backstory
     print("Do you want to hear the backstory?")
+
+    //Conditional to let the user choose if they want to hear the backstory
     if let hearBackStory = readLine() {
-       //Conditionals---> If or else are the conditional statements, if user says yes to backstory they get a backstory if else, they move to te main plot.
+
+        //Condition for if the user does want to hear hear the backstory
         if (hearBackStory == "yes") {
             backstory()
             print(" Tasha had much balance in her life, before dating and getting to know people. Also she was a family person she always understood the importance of family structure. However tasha love hard like most people and forgot about her weekly rountine working out and doing her therapy yogo. However her favorite food is seafood, she find herself very happy while watching her figure and staying at a weight of 175 before the break up.")
@@ -51,7 +58,9 @@ func chapterTwo() {
             print("Turns out he was abusive and Manipulator")
             chooseseafood(herfavoriteseafood: .Lobstertail)
             weeklyRountine(activity1: herWeeklyRoutine[0], activity2: herWeeklyRoutine[1], activity3: herWeeklyRoutine[2] )
-        } else {
+
+        //Condition for if the user doesnt want to hear te backstory
+        } else { 
             print(" Tasha had much balance in her life, before dating and getting to know people. Also she was a family person she always understood the importance of family structure. However tasha love hard like most people and forgot about her weekly rountine working out and doing her therapy yogo. However her favorite food is seafood, she find herself very happy while watching her figure and staying at a weight of 175 before the break up.")
             print("\(person1.name) love people that were down to earth")
             print("\(person1.name) meets a hood guy that started to ask for money and run game on her")
